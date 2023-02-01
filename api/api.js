7f40cf16f3eec6ab81c3b7a09d07933b0068cc8e
BeeMOre32 <windowssart01@gmail.com>
2023-02-01 14:07:27 +0900

    feat: user login and auth and logout and make api function
    when call api function it response the data

--- api/api.js
+++ api/api.js
@@ -1,5 +1,7 @@
-export const basicHeaders ={
-  "content-type": proocess.env.CONTENT_TYPE,
-  "apikey":  proocess.env.APIKEY,
-  "username":  proocess.env.CONTENT_TYPE,
-}
+export const basicHeaders = {
+  "content-type": process.env.CONTENT_TYPE,
+  apikey: process.env.APIKEY,
+  username: process.env.CONTENT_TYPE,
+};
+
+export const basicUrl = process.env.API_URL;
