5c4ebdf88a441abf0a80d34c6e199a0abbac8a15
BeeMOre32 <windowssart01@gmail.com>
2023-02-01 14:18:20 +0900

    conf: refactoring in api files and folder

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,6 +1,6 @@
 // Data는 반드시 객체 형태이고, api 문서 그대로 보내주어야 한다.
 export async function signIn(data) {
-  const res = await fetch("api/signin", {
+  const res = await fetch("api/account/signin", {
     method: "POST",
     body: JSON.stringify(data),
   });
@@ -8,7 +8,7 @@ export async function signIn(data) {
 }
 // Data는 반드시 객체 형태이고, api 문서 그대로 보내주어야 한다.
 export async function logIn(data) {
-  const res = await fetch("api/login", {
+  const res = await fetch("api/account" + "/login", {
     method: "POST",
     body: JSON.stringify(data),
   });
@@ -16,7 +16,7 @@ export async function logIn(data) {
 }
 // Data는 반드시 Access Token 형태로 보내주어야 하며, 반환 되는 값은 객체 형태이다.
 export async function userAuth(access) {
-  const res = await fetch("api/auth", {
+  const res = await fetch("api/account/auth", {
     method: "POST",
     headers: {
       authorization: "Bearer " + access,
@@ -26,7 +26,7 @@ export async function userAuth(access) {
 }
 
 export async function userLogOut(access) {
-  const res = await fetch("api/logout", {
+  const res = await fetch("api/account/logout", {
     method: "POST",
     headers: {
       authorization: "Bearer " + access,
@@ -36,7 +36,7 @@ export async function userLogOut(access) {
 }
 
 export async function userInfoEdit(data) {
-  const res = await fetch("api/userinfoedit", {
+  const res = await fetch("api/account/userinfoedit", {
     method: "PUT",
     headers: { authorization: "Bearer " + data.userToken },
     body: JSON.stringify(data.user),
