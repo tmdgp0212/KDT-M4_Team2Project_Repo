590e4e089e9c725b543a668704ccc34367ee6370
seunghye <tmdgp0212@naver.com>
2023-02-15 13:45:07 +0900

    feat: link commonjs on every routes

--- src/page/search.js
+++ src/page/search.js
@@ -1,3 +1,25 @@
-export function renderSearchPage() {
-  console.log('ggsearcch')
+import { searchProduct } from "../utilities/productapi";
+import { router } from "../route";
+
+export async function renderSearchPage(params) {
+  const app = document.querySelector('#app');
+  const searchText = params.data.query;
+
+  app.innerHTML = /* html */`
+    <div class="container search-page">
+      <h1><span>'${searchText}'<span>로 검색한 결과입니다</h1>
+
+      <div class="search--result">
+        <ul>
+          <li class="item">
+            <div class="item--image"></div>
+            <div class="item--info"></div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `;
+
+  const matchItems = await searchProduct({searchText});
+  console.log(matchItems);
 }
--- src/route.js
+++ src/route.js
@@ -1,4 +1,5 @@
 import Navigo from "navigo";
+import { CommonFn } from "./common";
 import { renderMasterPage } from "./page/master";
 import { renderAddProduct } from "./page/massteraddproduct";
 import { renderMainPage } from "./page/main";
@@ -12,63 +13,77 @@ export const router = new Navigo("/");
 const app = document.querySelector("#app");
 
 router.on("/", function () {
+  CommonFn();
   renderMainPage();
 });
 
 router.on("/search/:query", function (params) {
-  console.log(params.query);
-  renderSearchPage();
+  CommonFn();
+  renderSearchPage(params);
 });
 
 router.on("/product/detail/:productId", function (params) {
+  CommonFn();
   renderDetailPage(params);
 });
 
 router.on("/product/all", function () {
+  CommonFn();
   renderAllProducts("all");
 });
 
 router.on("/product/best", function () {
+  CommonFn();
   renderAllProducts("best");
 });
 
 router.on("/product/new", function () {
+  CommonFn();
   renderAllProducts("new");
 });
 
 router.on("/product/cart", function () {
+  CommonFn();
   console.log("cart");
 });
 
 router.on("/product/checkout", function () {
+  CommonFn();
   console.log("checkout");
 });
 
 router.on("/mypage", function () {
+  CommonFn();
   app.innerHTML = `<h1>My Page</h1>`;
 });
 
 router.on("/mypage/bank", function () {
+  CommonFn();
   console.log("mypage/bank");
 });
 
 router.on("/login", function () {
+  CommonFn();
   console.log("login");
 });
 
 router.on("/master", function () {
+  CommonFn();
   renderMasterPage();
 });
 
 router.on("/master/product/detail/:id", function (match) {
+  CommonFn();
   renderMasterProductDetailPage(match.data.id);
 });
 
 router.on("/master/product/add", function () {
+  CommonFn();
   renderAddProduct();
 });
 
 router.on("/master/sold", function () {
+  CommonFn();
   renderSoldProduct();
 });
 
